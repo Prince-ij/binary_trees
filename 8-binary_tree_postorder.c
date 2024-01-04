add5207b26def3da63caa71d7099b0aa2eae3bf7
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-04 11:21:27 -0800

    8-binary_tree_postorder.c

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - Function that traverses in LRN form
+ * @tree - tree to be traversed
+ * @func: what to do at each traversal
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)){
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
