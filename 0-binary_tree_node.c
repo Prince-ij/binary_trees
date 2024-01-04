0770c4a2a31523eee3b006eabd263d1c41520359
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-03 22:42:33 -0800

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,9 +15,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	if (parent == NULL)
-		return (NULL);
-
 	newNode->parent = parent;
 	newNode->left = NULL;
 	newNode->right = NULL;
