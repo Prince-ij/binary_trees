487a5c1d6ac3d43489b3cb673ae15f3cd15df5cc
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-04 02:55:04 -0800

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,8 +8,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->parent == NULL)
-		return (1);
+
 	if (node->left == NULL && node->right == NULL)
 		return (1);
+	else
+		return (0);
 }
