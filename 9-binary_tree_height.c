69d94df1cc09c9ebecb23f37acd6ac408d416d46
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-04 15:55:17 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,17 +6,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	int left_h, right_h;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left): 0;
 
-	right_height = binary_tree_height(tree->right);
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	if (left_height > right_height)
-		return (left_height);
-	else
-		return (right_height);
+	return ((left_h > right_h) ? (left_h) : (right_h));
 }
