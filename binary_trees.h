1de5626405f87844e4d47b48d69ad0784dbc87a7
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-03 22:49:47 -0800

    0-binary_tree_node.c

--- binary_trees.h
+++ binary_trees.h
@@ -17,7 +17,7 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
-#include <stdio h>
+#include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 typedef struct binary_tree_s binary_tree_t;
