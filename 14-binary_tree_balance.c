938070acf23bf93e8b1022b670dccde8dccbd757
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-04 17:24:30 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - Function to calculate height of a tree
+ * @tree: tree to be calculated
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return ((left_h > right_h) ? (left_h) : (right_h));
+}
+/**
  * binary_tree_balance - Calcs diff btw height of left & right nodes
  *
  * @tree: given tree
@@ -7,29 +23,9 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	size_t l_h = binary_tree_height(tree->left);
-	size_t r_h = binary_tree_height(tree->right);
-
-	return (l_h - r_h);
-}
-/**
- * binary_tree_height - Function to calculate height of a tree
- * @tree: tree to be calculated
- * Return: height of tree
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-        int left_h, right_h;
-
-        if (tree == NULL)
-                return (0);
-
-        left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-
-        right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-        return ((left_h > right_h) ? (left_h) : (right_h));
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
 }
