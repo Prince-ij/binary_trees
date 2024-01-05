b1fbe5a0a4b3cb1d3a1910de6930101ec1018b23
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-04 17:44:28 -0800

    15-binary_tree_is_full.c

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - Checks for 0 or 2 children
+ * @tree: given tree
+ * Return: 1 for success 0 for failure
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if ((!tree->left && !tree->right) || (tree->left && tree->right))
+		return (1);
+	else
+		return (0);
+}
