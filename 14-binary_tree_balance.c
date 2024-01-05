0d0d90df8c7cfb84376af45af82794d34f50714c
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-04 16:41:58 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,3 +15,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (l_h - r_h);
 }
+/**
+ * binary_tree_height - Function to calculate height of a tree
+ * @tree: tree to be calculated
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        int left_h, right_h;
+
+        if (tree == NULL)
+                return (0);
+
+        left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+
+        right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+        return ((left_h > right_h) ? (left_h) : (right_h));
+}
