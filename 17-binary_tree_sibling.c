116b62774ceed2a09dd747b91bee6daebdf0fe53
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-05 09:26:03 -0800

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,10 +7,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
 	binary_tree_t *parent = node->parent;
 
-	if (node == NULL || parent == NULL)
-		return (NULL);
 	if (parent->left == node && parent->right != NULL)
 		return (parent->right);
 	else if (parent->right == node && parent->left != NULL)
