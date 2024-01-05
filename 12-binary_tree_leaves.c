2ab39c7f25d4c70759da90dc5d0a3dae9655a90f
Prince-ij <aminuibrahimthefifth@gmail.com>
2024-01-04 16:21:32 -0800

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,11 +8,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
 
 	size_t left_leaf = binary_tree_leaves(tree->left);
 	size_t right_leaf = binary_tree_leaves(tree->right);
 
-	return (left_leaf + right_leaf);
+	size_t leaves = left_leaf + right_leaf;
+
+	return (!left_leaf && !right_leaf ? leaves + 1: leaves);
 }
